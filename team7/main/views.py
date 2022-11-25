60b2c0575149646c4233a9dc83c13faa76913736
albert-caffrey <wnstn82612@gmail.com>
2022-11-25 23:51:51 +0900

    test2

--- team7/main/views.py
+++ team7/main/views.py
@@ -4,5 +4,3 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
-def test(request):
-    return render(request, 'test.html')
--- team7/team7/urls.py
+++ team7/team7/urls.py
@@ -20,5 +20,4 @@ from main import views as mainview
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainview.index, name='index'),
-    path('admin2/',mainview.test),
 ]
