d9b68cacf22c41bcd5869233b91c24a788bb54c1
niceyoo0221 <yoo020221@naver.com>
2022-12-02 20:40:56 +0900

    login, register 등록

--- team7/accounts/views.py
+++ team7/accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def create_user(request):
+    id = request.POST['username']
+    password = request.POST['username']
+    first_name = request.POST['username']
+    last_name = request.POST['username']
+    sex = request.POST['sex']
+    return redirect('index')
--- team7/team7/urls.py
+++ team7/team7/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main import views as mainview
 
 urlpatterns = [
@@ -30,6 +30,7 @@ urlpatterns = [
     path('courseSearchbyKey/',mainview.courseSearchbyKey, name='courseSearchbyKey'),
     path('registComplete/',mainview.registComplete, name='registComplete'),
     path('test/', mainview.test, name='test'),
+    path('account/', include('accounts.urls'))
 ]
 
 # from django.conf.urls.static import static
