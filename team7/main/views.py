b1212fc34ed0400b3071f03e98c50bbc60e31353
niceyoo0221 <yoo020221@naver.com>
2022-12-06 04:56:37 +0900

    course 및 place detail, 댓글 작성 기능 완료

--- team7/main/views.py
+++ team7/main/views.py
@@ -249,36 +249,24 @@ def courseRegSubmit(request):
 
 @login_required
 def courseCommentRegist(request,courseid):
-    comment = request.POST['comment_txt']
+    comment = request.POST['course-comment']
     query = "select max(commentid) from course_comment"
+    max_commentid = 0
     cursor.execute(query)
     for rows in cursor:
         max_commentid = int(rows[0])
-    maker_id = 'user1'
+
+    maker_id = str(request.user)
+    
     time = timezone.localtime()
     #'2021-01-01 19:18:45
     created_time = time.strftime('%Y-%m-%d')+" "+time.strftime('%X')
     #created_time = str(time[0])+'-'+str(time[1])+'-'+str(time[2])+' '+str(time[3])+':'+str(time[4])+':'+str(time[5])
     query = ("insert into course_comment values ("+ str(max_commentid+1)+",'"
-            + comment+"','"+maker_id+"',"+ "to_date('"+created_time+"', "+"'yyyy-mm-dd hh24:mi:ss'),"+str(courseid)+")")
-    # print(query)
+            + comment+"','"+maker_id+"',"+ "to_date('"+created_time+"', 'yyyy-mm-dd hh24:mi:ss'),"+str(courseid)+")")
     cursor.execute(query)
     connect.commit()
-    query = ("select C.name, P.name,P.location_city,P.location_district,P.location_street, P.location_address, P.placeid, C.courseid" + 
-            " from (course C join course_consist CC on C.courseID=CC.courseID)" +
-             " join place P on P.placeID=CC.placeID where C.courseID ="+ str(courseid))
-    cursor.execute(query) 
-    list1 = []
-    for rows in cursor:
-        list1.append(rows)  
-    coursename = rows[0]
-    query = "select text, author, created_time from course_comment where courseid = " + str(courseid)
-    cursor.execute(query) 
-    list2 = []
-    for rows in cursor:
-        list2.append(rows)
-    return render(request, 'courseDetail.html',{ 'coursename':coursename,'list1':list1,'list2':list2,'courseid':courseid})
-
+    return redirect('courseDetail', courseid=courseid)
 
 @login_required
 def placeCommentRegist(request, placeid):
